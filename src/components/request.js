1816cd9716ecd0187dc758d5f80fee1cd3127d0d
Lance Godard <lancegodard@Lances-MBP.attlocal.net>
2023-10-26 13:42:13 -0400

    major updates to output

--- src/components/request.js
+++ src/components/request.js
@@ -17,13 +17,41 @@ class RequestData extends Component {
       generatedResponse3: "",
       generatedResponse4: "",
       generatedResponse5: "",
+      generatedResponse6: "",
+      loadingMessages: [
+        "We're analyzing your audience...",
+        "We're extracting key takeaways...",
+        "We're drafting alternative titles...",
+        "We're writing an abstract...",
+        "We're crafting a social media post...",
+        "We're generating an executive summary..."
+      ],
+      currentLoadingMessage: "",
       isLoading: false,
       error: null,
     };
-  }
+  };
+
+  componentDidMount() {
+    this.loadingMessageTimer = setInterval(this.changeLoadingMessage, 5000);
+    this.setState({currentLoadingMessage: "We're reviewing your content..."});
+    setTimeout(this.changeLoadingMessage, 5000);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.loadingMessageTimer);
+  };
+
+  changeLoadingMessage = () =>{
+    const {loadingMessages} = this.state;
+    const randomIndex = Math.floor(Math.random() * loadingMessages.length);
+    const newMessage = loadingMessages[randomIndex];
+    this.setState({currentLoadingMessage: newMessage});
+  };
+
   handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ isLoading: true, questionDisplay: false });
+    this.setState({ isLoading: true, questionDisplay: false, responseDisplay: false});
     console.log("sending request...")
     const requestData = {
       title: this.state.articleTitle,
@@ -120,7 +148,7 @@ class RequestData extends Component {
           messages: [
             {
               role: "user",
-              content: `Provide a compelling 10 word summary of "${requestData.title}" that can be used to promote the article on Twitter, LinkedIn, and other social media.`,
+              content: `Provide a compelling summary of "${requestData.title}" that can be used to promote the article on Twitter, LinkedIn, and other social media. Include at least three appropriate hashtags. The entire post, including spaces and hashtags, should be no more than 120 characters.`,
             },
           ],
         },
@@ -132,6 +160,30 @@ class RequestData extends Component {
         }
       );
 
+      const response6 = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: `Please provide a 200-word abstract of "${requestData.copy}".`,
+            },
+          ],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          },
+        }
+      );
+
+      console.log(response6);
+      console.log("Response Data 6:", response6.data);
+
+
+
       console.log(response3);
       console.log("Response Data 3:", response3.data);
 
@@ -140,12 +192,14 @@ class RequestData extends Component {
       const generatedResponse3 = response3.data.choices[0].message.content;
       const generatedResponse4 = response4.data.choices[0].message.content;
       const generatedResponse5 = response5.data.choices[0].message.content;
+      const generatedResponse6 = response6.data.choices[0].message.content;
       const generatedResponse =
         generatedResponse1 +
         generatedResponse2 +
         generatedResponse3 +
         generatedResponse4 +
-        generatedResponse5;
+        generatedResponse5 +
+        generatedResponse6;
 
       console.log("Generated Responses: ", generatedResponse);
       this.setState({
@@ -157,6 +211,7 @@ class RequestData extends Component {
         generatedResponse3,
         generatedResponse4,
         generatedResponse5,
+        generatedResponse6,
       });
 
       this.setState({
@@ -194,12 +249,14 @@ class RequestData extends Component {
   };
 
   render() {
+    const {currentLoadingMessage} = this.state;
     const {
       generatedResponse1,
       generatedResponse2,
       generatedResponse3,
       generatedResponse4,
       generatedResponse5,
+      generatedResponse6,
       isLoading,
       error,
     } = this.state;
@@ -209,7 +266,7 @@ class RequestData extends Component {
         <div className="container-fluid">
           {isLoading && (
             <div className="isLoading">
-            <h3>We're analyzing your content...</h3>
+            <h3>{currentLoadingMessage}</h3>
             </div>
           )}
         </div>
@@ -248,6 +305,11 @@ class RequestData extends Component {
                   <option value="3"></option>
                   <option value="4"></option>
                   <option value="5"></option>
+                  <option value="6"></option>
+                  <option value="7"></option>
+                  <option value="8"></option>
+                  <option value="9"></option>
+                  <option value="10"></option>
                 </datalist>
                 <input
                   list="totalPoints"
@@ -299,16 +361,24 @@ class RequestData extends Component {
                 </ul>
               </p>
               <p>
-                <b>Email-Ready Synopsis</b>
+                <b>Executive Summary (under 25 words)</b>
                 <br />
                 {generatedResponse4} 
               </p>
               <p>
-                <b>Social Media Post</b>
+                <b>Abstract (approximately 200 words)</b>
+                <br />
+                 {generatedResponse6.split('\n').map((paragraph) => (
+                    <p>{paragraph.replace(/^\d+\.\s*/g, '')}</p>
+                    ))}
+                    </p>
+              <p>
+                <b>Social Media Post (under 120 characters)</b>
                 <br />
                 {generatedResponse5}
               </p>
               <button onClick={this.handleNewRequest}>New Request</button>
+              <button onClick={this.handleSubmit}>Regenerate Reponse</button>
             </div>
           )}{" "}
         {this.state.error && (
